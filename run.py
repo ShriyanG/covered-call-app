5f0c0a8ca59cc7daa1a0a8f66b44b31305f0614a
ShriyanG <shriyan.gosavi@gmail.com>
2025-09-15 23:17:40 -0500

    Added port check code.

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 from app.routes import main
@@ -6,4 +8,5 @@ app = Flask(__name__)
 app.register_blueprint(main)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
